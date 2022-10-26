bc9277d8f97719815abd2d85ac891abc26089772
Weigang Qiu <wgqiu@github.com>
2022-10-26 10:53:36 -0400

    adding search results

--- shell-scripts/cov_sim.sh
+++ shell-scripts/cov_sim.sh
@@ -5,6 +5,6 @@ land_file=$1
 
 for alg in {1..3}; do
 	for trial in {1..30}; do
-		python ${exe_dir}/aa_search.py -t alg$alg-trial$trial -land $land_file -alg $alg
+		python ${exe_dir}/cov_search.py -t alg$alg-trial$trial -land $land_file -alg $alg
 	done
 done
