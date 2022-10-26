d52bc89a1b5201e8f8eae345b9ae3acae45d8b95
winstonk6 <54592892+winstonk6@users.noreply.github.com>
2022-10-26 07:22:35 -0400

    Added basin of attraction function

--- python-scripts/land-stats.py
+++ python-scripts/land-stats.py
@@ -17,6 +17,7 @@ from sklearn.linear_model import LinearRegression
 import sys
 import logging
 import copy
+import networkx as nx
 
 # Parameters -------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='Landscape complexity scores')
@@ -25,6 +26,7 @@ parser.add_argument('-l', '--hap_len', action = 'store_true', help='Number of va
 parser.add_argument('-n', '--hap_num', action = 'store_true', help='Number of haplotypes')
 parser.add_argument('-p', '--peaks', action = 'store_true', help='Maximum number of peaks')
 parser.add_argument('-rs', action = 'store_true', help='Roughness')
+parser.add_argument('-b', action = 'store_true', help='Basin of attraction')
 
 args = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG)
@@ -86,5 +88,81 @@ if args.peaks is True:
 
 if args.rs is True:
     print(cal_r_s(landscape))
+
+# Calculate basin of attraction
+def fit_diff(G, nd, suc):
+    return G.nodes[suc]['fit'] - G.nodes[nd]['fit'] # must be positive
+
+# recursive function
+def greedy_climb(G, nd, pt):
+    if nd in peaks: # a peak, no successors; but inclusive as a basin hap
+        pt.append(nd)
+        return
+    best = sorted( [suc for suc in G.successors(nd)], key = lambda x: fit_diff(G, nd, x), reverse = True) # for each m -> n
+    pt.append(best[0])
+    greedy_climb(G, best[0], pt)
+
+if args.b is True:
+    # Construct graph
+    recs = df.to_dict("records")
+    dHap = {}
+    for rec in recs:
+        dHap[rec['haplotype']] = {'id': rec['id'], 'fit': rec['fitness']}
+
+    DG = nx.DiGraph() 
+    for hap in dHap: # hap as key
+        DG.add_node(hap)
+        for k in dHap[hap]: # k includes 'id' and 'fit'
+            DG.nodes[hap][k] = dHap[hap][k]
+        DG.nodes[hap]['alpha'] = dHap[hap]['fit'] # assign fitness as alpha (transparency)
     
+    def add_fit_edge(G, hap, seen, level):
+        num_zeros = len([x for x in hap if x == '0'])
+        if num_zeros == 0: # reached the end hap (all 1's)
+            seen[hap] = 1
+            return
+        if hap in seen_nodes: # node already reached
+            return
+        else: # contains at least one zero & not seen
+            seen[hap] = 1
+            G.nodes[hap]['subset'] = level # networkx uses node feature 'subset' to store levels for multipartite layout
+            nabes = [] # start a list of 1-hamming neighbors
+            for i in range(len(hap)): # mutate each 0 to 1
+                if hap[i] == '0':
+                    mut = hap[0:i] + '1' + hap[(i+1):]
+                    if G.nodes[hap]['fit'] > G.nodes[mut]['fit']:
+                        G.add_edge(mut, hap)
+                    else:
+                        G.add_edge(hap, mut)
+                    nabes.append(mut)
+            level += 1
+            for x in nabes: # recurse on each child node
+                if x not in seen: # skip if seen
+                    G.nodes[x]['subset'] = level
+                    add_fit_edge(G, x, seen, level)
+    seen_nodes = {}
+    add_fit_edge(DG, '0000000000', seen_nodes, 0) # start point
+
+    # node coloring by fitness 
+    node_alphas = [ DG.nodes[x]['fit'] for x in DG.nodes]
+    peaks = [x[0] for x in DG.out_degree if x[1] == 0]
+    vals = [x[0] for x in DG.in_degree if x[1] == 0]
+
+    # Basin of attraction (should partition the haplotype space)
+    basins = {} # nuumber of paths to a peak (through greedy hill climbing)
+    for p in peaks: # initalize dict
+        basins[p] = []
+
+    for nd_i in DG.nodes: # 1024 starting nodes
+        #start = np.random.default_rng().choice(DG.nodes)
+        path = []
+        greedy_climb(DG, nd_i, path) # recursive until a peak
+        peak = path[-1] # last item is the peak
+        basins[peak].append(path)
+        #print(path)
+
+    print("id", "\t", "haplotype", "\t", "basin_size", "\t", "fitness")
+    for p in basins:
+        print(dHap[p]['id'], "\t", p, "\t", len(basins[p]), "\t", round(dHap[p]['fit'],6))
+
 sys.exit()
